11835feb4f24354b7fbe4dd1d4de033b09b96028
szkoti <120187555+szkoti@users.noreply.github.com>
2024-05-29 16:06:23 +0200

    added counter from 5

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,14 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+
+if (randomNumber > 5) {
+    for (let i = 5; i < randomNumber +1; i++) {
+        let array = [];
+        array += i;
+        console.log(array);
+    }
+
+} else {
+    console.log("Wylosowana liczba jest zbyt mała, aby użyć pętli");
+}
