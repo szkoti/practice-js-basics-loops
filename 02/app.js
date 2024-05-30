d2a04f89e58883b0b55aa0d40cbb2cdcef3ac975
szkoti <120187555+szkoti@users.noreply.github.com>
2024-05-30 19:30:37 +0200

    added while loop

--- 02/app.js
+++ 02/app.js
@@ -2,7 +2,14 @@ const x = 5;
 let result = 0;
 
 
-for (let i = 1; i < x + 1 ; i++) {
+// for (let i = 1; i < x + 1 ; i++) {
+//     result = i + result;
+//     console.log(result);
+// };
+
+let i = 1;
+while(i <= x) {
     result = i + result;
-    console.log(result);
+    i++;
 }
+console.log(result)
